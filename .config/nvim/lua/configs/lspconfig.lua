203900aaf7edfc21d7df5c71a6c792bda2d2e52f
PositronSpin <positronspin@outlook.com>
2025-11-11 11:47:37 +0800

    rename tmux.conf & fix lspconfig.lua

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { }
+local servers = { "clangd" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
