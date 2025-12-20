74e625ff5f6dcfe9b130fbe486092f8f5e5375a7
PositronSpin <positronspin@outlook.com>
2025-12-20 16:35:10 +0800

    update

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "clangd", "hls" }
+local servers = { "clangd", "hls", "omnisharp" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
